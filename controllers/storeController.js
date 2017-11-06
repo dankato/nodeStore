f4e02be87be8133e2a34183609c6cbe9e0d4b2b4
dankato <apearmor@gmail.com>
2017-11-06 00:04:42 -0800

    querying db for stores but last line in _storeCard.pug, its broken

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -17,3 +17,10 @@ exports.createStore = async (req, res) => {
     res.redirect(`/store/${store.slug}`);
     // console.log('saved to db');
 }
+
+exports.getStores = async (req, res) => {
+    // 1. query the db for a list of all stores
+    const stores = await Store.find();
+    console.log(stores)
+    res.render('stores', {title: 'Stores', stores: stores});
+}
